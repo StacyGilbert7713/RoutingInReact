fcdcaf24aebd451d690e6b745cf5f38bfde945d7
Stacy Gilbert <StacyGilbert7713@gmail.com>
2020-09-27 23:02:30 -0400

    Jeopardy Lite

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { Route } from "react-router-dom";
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import "./App.css";
 
+import "./App.css";
 import Welcome from "./components/welcome/Welcome";
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Navigation from "./components/navigation/Navigation";
 import Page404 from "./components/page404/Page404.js";
+import Jeopardy from "./components/jeopardy/Jeopardy.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
-
         <Navigation />
-
         <Switch>
           <Route
             exact path="/"
-            render={(props) => <Welcome {...props} name="Friend!" />}
-
+            render={(props) => <Welcome {...props} name="Stacy!" />}
           />
-
 
           <Route
             path="/welcome/:name"
             render={(props) => <Welcome {...props} name={props.match.params.name} />}
           />
-
 
           <Route
             exact path="/clock"
@@ -40,12 +36,16 @@ function App() {
             component={Contact}
           />
 
+          <Route 
+            exact path="/jeopardy"
+            component={Jeopardy}
+          />
+
           <Route
             component={Page404}
           />
 
         </Switch>
-
       </div>
     </Router>
   );
---
+++ src/components/jeopardy/gameDisplay.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function JeopardyDisplay(props) {
+    return (
+        <div>
+            <strong>Score: </strong>{props.score} <br /><br />
+            <strong>Question: </strong>{props.data.question} <br />
+            <strong>Value: </strong>{props.data.value} < br />
+            <strong>Category: </strong>{props.data.category.title}<br />
+            <strong>Your Answer: </strong>{props.userAnswer} <br /><br />
+            <div id="jeopardy_answer">
+                <input type="text" onChange={props.changeAnswer} size="30" name="answer" placeholder="Enter your answer"></input>
+                <button onClick={props.handleAnswer}>Submit Answer</button>
+            </div>
+        </div>
+    );
+}
+
+export default JeopardyDisplay;
